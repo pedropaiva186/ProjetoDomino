d213152dd72e48b14d0cee810cfcfae215acd263
Kevin Gabriel <166726818+Melvin2781@users.noreply.github.com>
2025-04-06 12:26:55 -0300

    Adição de métodos no tabuleiro
    -- Mudança no construtor da classe, agora os jogadores são criados na inicialização.
    -- Adição de alguns métodos get.
    --Adição do método para setar o baralho de um player.

--- Main.java
+++ Main.java
@@ -4,15 +4,12 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args){
-        Tabuleiro tabuleiro1 = new Tabuleiro();
+        Tabuleiro tabuleiro1 = new Tabuleiro("Kevin");
         List<Pedra> dorme = tabuleiro1.getDorme();
-
+        tabuleiro1.setBaralho(tabuleiro1.getPlayer(0));
+        
         for(int i = 0; i < dorme.size(); i++) {
-            System.out.printf("%d %d\n", (int)dorme.get(i).getNumCima(), (int) dorme.get(i).getNumBaixo());
-        }
-
-        for(Pedra pedra: dorme) {
-            System.out.printf("%d %d\n", (int)pedra.getNumCima(), (int) pedra.getNumBaixo());
+            System.out.printf("%d - %d %d\n", i, (int)dorme.get(i).getNumCima(), (int) dorme.get(i).getNumBaixo());
         }
     }
 }
--- modelo/Tabuleiro.java
+++ modelo/Tabuleiro.java
@@ -9,8 +9,12 @@ public class Tabuleiro {
     private List<Pedra> pedrasDireita = new ArrayList<>();
     private Jogador[] jogadores = new Jogador[4];
 
-    public Tabuleiro(){
+    public Tabuleiro(String nome){
         setDorme();
+        jogadores[0] = new Humano(nome);
+        for(int i = 1; i <= 3; i++) {
+            jogadores[i] = new Maquina(String.format("bot %d", i));
+        }
     }
 
     // Método responsável pela criação das pedras do dorme. Caso a operação seja realizada com sucesso, retorna true; caso contrário, retorna false.
@@ -35,6 +39,26 @@ public class Tabuleiro {
     public List<Pedra> getDorme() {
         return dorme;
     }
+    // Método responsável por preencher o deck de um player.
+    public void setBaralho(Jogador player) {
+        Random randomizer = new Random();
+        for(int i = 0; i <= 6; i++) {
+            int index = randomizer.nextInt(dorme.size());
+
+            player.addPedra(dorme.get(index));
+            dorme.remove(index);
+        } 
+    }
+    // Método responsável por retornar a referência a um player.
+    public Jogador getPlayer(int index) {
+        Jogador retorno = null;
+        try{
+            retorno = jogadores[index];
+        }catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Índice inválido");
+        }
+        return retorno;
+    }
 
     // Método responsável por colocar o valor das pontas do tabuleiro.
     public void setPontas(){
@@ -43,5 +67,10 @@ public class Tabuleiro {
 
         pontas[0] = pedrasDireita.get(finalDireita).getNumCima();
         pontas[1] = pedrasEsquerda.get(finalEsquerda).getNumBaixo();
+    }
+
+    // Método responsável por retornar o array formado pelas pontas do tabuleiro.
+    public int[] getPontas(){
+        return pontas;
     }
 }
