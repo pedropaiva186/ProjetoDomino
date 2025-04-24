b4b662792aadf64f17318dcadfeacacbab0654ce
Kevin <kevinmangueira@gmail.com>
2025-04-24 08:04:21 -0300

    Mudanças no método iniciarPartida

--- modelo/Tabuleiro.java
+++ modelo/Tabuleiro.java
@@ -59,7 +59,7 @@ public class Tabuleiro {
     // Método responsável por preencher o deck de um player.
     private void setBaralho(Jogador player) {
         Random randomizer = new Random();
-        for(int i = 0; i <= 6; i++) {
+        for(int i = 0; i < 6; i++) {
             int index = randomizer.nextInt(dorme.size());
 
             player.addPedra(dorme.get(index));
@@ -109,9 +109,9 @@ public class Tabuleiro {
     }
 
     // Método responsável por buscar a sena no baralho de um jogador.
-    private boolean acharSena(List<Pedra> pedras, int indexPlayer) {
+    private boolean acharCarroca(int cabeca, List<Pedra> pedras, int indexPlayer) {
         for(Pedra pedra: pedras) {
-            if(pedra.getNumCima() == 6 && pedra.getNumBaixo() == 6) {
+            if(pedra.getNumCima() == cabeca && pedra.getNumBaixo() == cabeca) {
                 pedrasTabuleiro.add(pedra);
                 pedras.remove(pedra);
                 
@@ -127,16 +127,17 @@ public class Tabuleiro {
     public void iniciarPartida() {
         boolean achou;
 
-        for(int i = 0; i < 4; i++) {
-            List<Pedra> pedras = jogadores[i].getPedras();
+        for(int i = 6; i > 1; i--) {
+            for(int j = 0; j < 4; j++) {
+                List<Pedra> pedras = jogadores[i].getPedras();
 
-            achou = acharSena(pedras, i);
+                achou = acharCarroca(i, pedras, j);
 
-            if(achou) {
-                break;
+                if(achou) {
+                    break;
+                }
             }
         }
-
         passes = 0;
         rodadas++;
         setPontas();
